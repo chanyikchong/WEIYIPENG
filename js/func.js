9d5db52b687140bce60c6fc17b2e9762b673d642
chanyikchong <1642508849@qq.com>
2020-10-25 11:47:22 +0800

    add file

---
+++ js/func.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+  }
+  
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
+
+function check(){
+    console.log(screen.width)
+}
+check()

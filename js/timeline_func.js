0cc0b0a9cd99173c88825c5e9d8b0dbd4fc93a71
chanyikchong <1642508849@qq.com>
2020-11-02 11:13:21 +0800

    add files

---
+++ js/timeline_func.js
@@ -0,0 +1,37 @@
+function openNav() {
+    document.getElementById("mySidenav").style.height = "140px";
+    document.getElementById("menu_bar").style.display = "none"
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
+    document.getElementById("menu_bar").style.display = "block"
+  }
+
+function check_browser(){
+    var explorer = window.navigator.userAgent ;
+    if (explorer.indexOf("MSIE") >= 0) {
+        return 'ie';
+    }
+    else if (explorer.indexOf("Firefox") >= 0) {
+        return 'Firefox';
+    }
+    else if(explorer.indexOf("Chrome") >= 0){
+        return 'Chrome';
+    }
+    else if(explorer.indexOf("Opera") >= 0){
+        return 'Opera';
+    }
+    else if(explorer.indexOf("Safari") >= 0){
+        return 'Safari';
+    }
+}
+window.onload=function slideshow() {
+    if (check_browser() === 'Firefox' || check_browser() === 'Safari'){
+        document.getElementById('logo').className = "headerLogo_fire";
+    }
+    // else if (check_browser() === 'Chrome' || check_browser() === "Edge")
+    else {
+        document.getElementById('logo').className = "headerLogo_chrom";
+    }
+}

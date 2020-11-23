6c701fcb84fbbd8d5e90a9177e5ca3d8fce01546
chanyikchong <1642508849@qq.com>
2020-11-23 11:27:28 +0800

    correct moon phase

--- js/home_func.js
+++ js/home_func.js
@@ -27,9 +27,41 @@ function check_browser(){
         return 'Safari';
     }
 }
+
+function getMoonPhase(year, month, day)
+{
+    var c = e = jd = b = 0;
+    if (month < 3) {
+        year--;
+        month += 12;
+    }
+    ++month;
+    c = 365.25 * year;
+    e = 30.6 * month;
+    jd = c + e + day - 694039.09; //jd is total days elapsed
+    jd /= 29.5305882; //divide by the moon cycle
+    b = parseInt(jd); //int(jd) -> b, take integer part of jd
+    jd -= b; //subtract integer part to leave fractional part of original jd
+    b = Math.round(jd * 8); //scale fraction from 0-8 and round
+    if (b >= 8 ) {
+        b = 0; //0 and 8 are the same so turn 8 into 0
+    }
+    // 0 => New Moon
+    // 1 => Waxing Crescent Moon
+    // 2 => Quarter Moon
+    // 3 => Waxing Gibbous Moon
+    // 4 => Full Moon
+    // 5 => Waning Gibbous Moon
+    // 6 => Last Quarter Moon
+    // 7 => Waning Crescent Moon
+    return b;
+}
 function set_moon(){
     var d = new Date();
     var date = d.getDate();
+    var month = d.getMonth() + 1;
+    var year = d.getFullYear()
+    var num = getMoonPhase(year, month, date)
     var waningCrescent = document.getElementById('moon0');
     var thirdQuarter = document.getElementById('moon1');
     var waningGibbous = document.getElementById('moon2');
@@ -38,34 +70,9 @@ function set_moon(){
     var firstQuarter = document.getElementById('moon5');
     var waxingCrescent = document.getElementById('moon6');
     var newMoon = document.getElementById('moon7');
-    var lis = [waningCrescent, thirdQuarter, waningGibbous, fullMoon, waxingGibbous, firstQuarter, waxingCrescent, newMoon]
-    function set_active(num){
-        lis[num].className['baseVal'] += 'active';
-    }
-    if (date >= 1 && date < 8){
-        set_active(2);
-    }
-    else if (date === 8){
-        set_active(1);
-    }
-    else if (date >= 9 && date < 15){
-        set_active(0);
-    }
-    else if(date === 15){
-        set_active(7);
-    }
-    else if(date >= 16 && date < 21){
-        set_active(6);
-    }
-    else if(date === 21){
-        set_active(5);
-    }
-    else if(date >= 22 && date < 30){
-        set_active(4);
-    }
-    else{
-        set_active(3);
-    }
+    var lis = [waningCrescent, thirdQuarter, waningGibbous, fullMoon, waxingGibbous, firstQuarter, waxingCrescent, newMoon];
+    lis = lis.reverse();
+    lis[num].className['baseVal'] += 'active';
 }
 
 window.onload=function slideshow() {
